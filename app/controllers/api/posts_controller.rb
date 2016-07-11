74fb744c92e05d95ceb45dbbd4d396a84c3bd635
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-07-10 23:23:43 -0500

    generate controllers for posts, users

---
+++ app/controllers/api/posts_controller.rb
@@ -0,0 +1,28 @@
+class Api::PostsController < ApplicationController
+
+def index
+end
+
+def show
+end
+
+def create
+end
+
+def update
+end
+
+def delete
+end
+
+end
+
+#
+#   namespace
+#   GE/api/posts to return all posts
+#   GET /api/posts/:id gives details of a post
+#   POST /api/posts creates a post
+#   PUT /api/posts/:id updates a post
+#   PATCH /api/posts/:id updates a post
+#   DELETE /api/posts/:id deletes a post
+# end
