937bea4565870541a1b5a86415ec3cf4d4c4bac8
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-07-11 00:14:31 -0500

    update post controller

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -10,17 +10,21 @@ def show
 end
 
 def create
-  @post = Post.new params.require(:title)
-  if @post.save
-    render :show
-  else
-    render json: @post.errors, status: 422
-  end
+   @post = Post.new
+   @post.title = params[:post][:title]
+   if @post.save
+     render :show, status: 201
+   else
+     render json: {errors: @post.errors}, status: 422
+   end
+
 end
+
+
 
 def update
   @post = Post.find_by id: params[:id]
-  if @post.update params.require(:title)
+  if @post.update title: params[:post][:title]
     render :show
   else
     render json: @post.errors, status: 422
